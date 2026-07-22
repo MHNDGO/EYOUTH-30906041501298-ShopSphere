b7dd2f785b9ff320fd1efb38b25e3e99dcae4f06
Mohanned Hemmat <63301158+MHNDGO@users.noreply.github.com>
2026-07-22 17:39:44 +0300

    Delete backend directory

--- backend/src/models/ActivityLog.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const activityLogSchema = new mongoose.Schema({
-  userId: { type: String, default: null },
-  action: { type: String, required: true },
-  meta: { type: mongoose.Schema.Types.Mixed, default: {} },
-}, { timestamps: true });
-
-module.exports = mongoose.model('ActivityLog', activityLogSchema);
--- backend/src/utils/cart.js
+++
@@ -1,5 +0,0 @@
-function calculateCartTotal(items) {
-  return items.reduce((sum, item) => sum + item.quantity * item.product.price, 0);
-}
-
-module.exports = { calculateCartTotal };
